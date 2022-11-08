3156f3f282860fa951aef90a3705e2fc705a354d
Bruce Scharlau <b.scharlau@abdn.ac.uk>
2022-11-08 19:32:03 +0000

    added files

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # RESTful_django_bears
-A RESTful version of the polar bears
+This application focuses on creating a RESTful version of the previous polar bears in django with forms repository so that we can add various front ends to consume JSON data. As before, this is an example of how you might do a RESTful application with Django.
+
+The goal of this session is to show how to run an application that serves both the HTML as before, plus, the JSON that can be consumed by another application.
+
+Start with the basics, pull down the repo and then set up your python envivonment and ensure Django is ready for use. 
+
+        pyenv local 3.10.7 # this sets the local version of python to 3.10.7
+        python3 -m venv .venv # this creates the virtual environment for you
+        source .venv/bin/activate # this activates the virtual environment
+        pip install --upgrade pip [ this is optional]  # this installs pip, and upgrades it if required.
+        pip install django
+        python3 manage.py migrate # to create the database and configure other parts of django
+        python3 manage.py parse_csv # to load the csv data into the database
+
+We'll use the Django REST framework https://www.django-rest-framework.org/ to supply the JSON serialisations/deserialisations and other hooks that we'll need in our application. As we're aiming to mix the JSON with the HTML, this shows one path through the various options of the framework. Go look at other parts of their tutorial to see how to create a version that serves only JSON too, if that is what you need.
+
+We can install it with this command:
+
+        pip install djangorestframework
+
+This enables us to do a number of things. First, we can build serialisers that map to our models.
+
+
+
---
+++ bears/views.py
@@ -0,0 +1,47 @@
+from django.utils import timezone
+from django.shortcuts import redirect, render, get_object_or_404
+from .models import Bear, Sighting
+from .forms import BearForm
+
+def bear_new(request):
+    if request.method=="POST":
+        form = BearForm(request.POST)
+        if form.is_valid():
+            bear = form.save(commit=False) # don't save yet, as want to add created_date
+            bear.created_date = timezone.now()
+            bear.save()
+            return redirect('bear_detail', id=bear.id) # use bear.id as we already have instance
+    else:
+        form = BearForm()
+    return render(request, 'bears/bear_edit.html', {'form': form}) #folder/file_name under 'templates' folder
+
+def bear_edit(request, id):
+    bear = get_object_or_404(Bear, id=id)
+    if request.method=="POST":
+        form = BearForm(request.POST, instance=bear)
+        if form.is_valid():
+            bear = form.save(commit=False)
+            bear.created_date = timezone.now()
+            bear.save()
+            return redirect('bear_detail', id=bear.id)
+    else:
+        form = BearForm(instance=bear)
+    return render(request, 'bears/bear_edit.html', {'form': form, 'bear': bear})
+
+def bear_delete(request, id):
+    bear = get_object_or_404(Bear, id=id)
+    bear.delete()
+    return redirect('bear_list' )
+
+def bear_list(request):
+    bears = Bear.objects.all()
+    return render(request, 'bears/bear_list.html', {'bears' : bears})
+
+def females(request):
+    females = Bear.female()
+    return render(request, 'bears/bear_list.html', {'bears' : females})
+
+def bear_detail(request, id):
+    bear = get_object_or_404(Bear, id=id)
+    sightings = Sighting.objects.filter(bear_id=id)
+    return render(request, 'bears/bear_detail.html', {'bear' : bear, 'sightings' : sightings})
